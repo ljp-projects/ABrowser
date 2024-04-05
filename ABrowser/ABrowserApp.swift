5a5b957a9b3489a78499a12b472e49703a650d3a
ljp-projects <lucasplumb@icloud.com>
2024-04-05 16:50:07 +1100

    Initial Commit

---
+++ ABrowser/ABrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABrowserApp.swift
+//  ABrowser
+//
+//  Created by Lucas on 5/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct ABrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
