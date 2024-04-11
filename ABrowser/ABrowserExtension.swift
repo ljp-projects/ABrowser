7543646ff0986922bc243eb9dbfc667f431f3334
ljp-projects <lucasplumb@icloud.com>
2024-04-12 07:16:23 +1000

    more stuff

--- ABrowser/ABrowserExtension.swift
+++ ABrowser/ABrowserExtension.swift
@@ -5,8 +5,91 @@
 //  Created by Lucas on 7/4/2024.
 //
 
-import Cocoa
+/*
+ URL to load from
+ 
+ E.g extension
+ */
 
-class ABrowserExtension: NSObject {
+import Foundation
+import WebKit
 
+struct ABrowserExtensionInfo {
+    let name: String
+    let author: String
+    let code: String
+    let description: String
+    let appliesTo: Regex<AnyRegexOutput>
+    
+    static func fromText(_ text: String) -> ABrowserExtensionInfo? {
+        
+        // Define regex patterns for each field
+        let nameRegex = try? Regex(#"name:\s*(.+)"#)
+        let authorRegex = try? Regex(#"author\(s\):\s*(.+)"#)
+        let appliesToRegex = try? Regex(#"applies-to:\s*(.+)"#)
+        
+        guard let nameMatch = try? nameRegex?.firstMatch(in: text),
+              let authorMatch = try? authorRegex?.firstMatch(in: text),
+              let appliesToMatch = try? appliesToRegex?.firstMatch(in: text) else {
+            return nil
+        }
+        
+        // Convert extracted values to appropriate types
+        let name = String(nameMatch[1].substring ?? "ABrowserExtension")
+        let author = String(authorMatch[1].substring ?? "")
+        let appliesTo = try? Regex(String(appliesToMatch[0].substring ?? ""))
+        
+        // Return the parsed struct
+        return ABrowserExtensionInfo(name: name, author: author, code: String(text.split(separator: "------")[1]), description: String(text.split(separator: "------")[2]), appliesTo: appliesTo!)
+    }
+}
+
+
+class ABrowserExtension {
+    var url: URL
+    let info: ABrowserExtensionInfo
+    
+    init(_ url: URL) {
+        self.url = url
+        self.info = .fromText(try! String(contentsOf: self.url))!
+    }
+    
+    func evaluate(_ webView: WKWebView) {
+        self.loadText { result in
+            switch result {
+            case .success(let result):
+                webView.evaluateJavaScript(result)
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    @discardableResult
+    private func loadText(_ completionHandler: @escaping (Result<String, (any Error)>) -> Void) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: URLRequest(url: self.url)) { data, response, error in
+            if error != nil {
+                // Client error
+                completionHandler(.failure(error!))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                // Server error
+                completionHandler(.failure(URLError(.unknown, userInfo: ["Code" : "520 UNKOWN", "Type" : "Server"])))
+                return
+            }
+            
+            if let mimeType = httpResponse.mimeType, mimeType == "text/plain",
+               let data = data,
+               let string = String(data: data, encoding: .utf8) {
+                completionHandler(.success(string))
+            }
+        }
+        
+        task.resume()
+        
+        return task
+    }
 }
--- ABrowser/ContentView.swift
+++ ABrowser/ContentView.swift
@@ -6,19 +6,289 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
+var urlString = "ecosia.org"
+let domainRegex = /\.(com|net|org|be|codes|io|co|us|ru|de|br|uk|jp|fr|it|edu|me|cn|ly|in|tv|ai|int|gov|app|mil)/
+let protocolRegex = /^\w+:\/\//
+
+struct ContentView: View, Hashable {
+    static func == (lhs: ContentView, rhs: ContentView) -> Bool {
+        lhs.webView == rhs.webView
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(webView)
+    }
+    
+    let webView = WebView()
+    
+    @State private var degrees = 0.0
+    @State private var loadingExt = false
+    @State private var prevExt = ""
+    var totalDuration = 0.25
+    
+    private var urlStringBind = Binding {
+        urlString
+    } set: { newValue in
+        urlString = newValue
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+#if os(macOS)
+            HStack {
+                Button(action: {
+                    webView.goBack()
+                }){
+                    Image(systemName: "arrow.backward")
+                        .font(.title)
+                        .padding()
+                }.buttonStyle(PlainButtonStyle())
+                
+                TextField("Enter url", text: urlStringBind)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .onSubmit {
+                        webView.loadURL(urlString)
+                    }
+                
+                Button(action: {
+                    webView.reload()
+                    
+                    withAnimation(.easeOut(duration: totalDuration)) {
+                        degrees -= 360
+                    }
+                }) {
+                    Image(systemName: "arrow.counterclockwise")
+                        .font(.title)
+                        .padding()
+                        .rotationEffect(.degrees(degrees))
+                    
+                    
+                }.buttonStyle(PlainButtonStyle())
+                
+                Button(action: {
+                    webView.goForward()
+                }) {
+                    Image(systemName: "arrow.forward")
+                        .font(.title)
+                        .padding()
+                    
+                    
+                }.buttonStyle(PlainButtonStyle())
+            }.onDrag {
+                NSItemProvider(object: webView.createURL(urlString)! as NSURL)
+            } preview: {
+                Text(urlString)
+            }
+            
+#endif
+            
+            // main webview
+            webView
+            
+#if os(iOS)
+            HStack {
+                Button(action: {
+                    webView.goBack()
+                }){
+                    Image(systemName: "arrow.backward")
+                        .font(.title)
+                        .padding()
+                }.buttonStyle(PlainButtonStyle())
+                
+                TextField("Enter url", text: urlStringBind)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .onSubmit {
+                        webView.loadURL(urlString)
+                    }
+                
+                Button(action: {
+                    webView.reload()
+                    
+                    withAnimation(.easeOut(duration: totalDuration)) {
+                        degrees -= 360
+                    }
+                }) {
+                    Image(systemName: "arrow.counterclockwise")
+                        .font(.title)
+                        .padding()
+                }.buttonStyle(PlainButtonStyle())
+                    .rotationEffect(Angle.degrees(degrees))
+                
+                Button(action: {
+                    webView.goForward()
+                }) {
+                    Image(systemName: "arrow.forward")
+                        .font(.title)
+                        .padding()
+                    
+                    
+                }.buttonStyle(PlainButtonStyle())
+            }.onDrag {
+                NSItemProvider(object: webView.createURL(urlString)! as NSURL)
+            } preview: {
+                Text(urlString)
+            }
+            
+#endif
         }
-        .padding()
+        .onAppear() {
+            webView.loadURL(urlString)
+        }
+        .onDrop(of: [.fileURL], isTargeted: nil) { providers in
+            if let provider = providers.first(where: { $0.canLoadObject(ofClass: URL.self) } ) {
+                let _ = provider.loadObject(ofClass: URL.self) { object, error in
+                    if let url = object {
+                        webView.loadURL(url.absoluteString)
+                    }
+                }
+                return true
+            }
+            return false
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
+
+#if os(macOS)
+struct WebView: NSViewRepresentable, Hashable {
+    typealias NSViewType = WKWebView
+    
+    
+    let webView: WKWebView
+    
+    init() {
+        self.webView = WKWebView()
+    }
+    
+    func makeNSView(context: Context) -> WKWebView {
+        webView.allowsBackForwardNavigationGestures = true
+        webView.configuration.upgradeKnownHostsToHTTPS = true
+        webView.configuration.allowsAirPlayForMediaPlayback = true
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
+        return webView
+    }
+    
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        if let url = nsView.url {
+            print(url.absoluteString)
+        }
+    }
+    
+    func goBack() {
+        webView.goBack()
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func goForward() {
+        webView.goForward()
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func reload() {
+        webView.reload()
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func loadURL(_ loadString: String) {
+        webView.load(URLRequest(url: self.createURL(loadString)!))
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func createURL(_ loadString: String) -> URL? {
+        if urlString.contains(protocolRegex) {
+            return URL(string: urlString)
+        } else if urlString.contains(domainRegex) {
+            return URL(string: "https://\(urlString)")
+        } else {
+           return URL(string: "https://ecosia.org/search?q=\(urlString)")
+        }
+    }
+}
+#else
+struct WebView: UIViewRepresentable, Hashable {
+    
+    let webView: WKWebView
+    
+    init() {
+        self.webView = WKWebView()
+        
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        webView.allowsBackForwardNavigationGestures = true
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+    
+    func goBack() {
+        webView.goBack()
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func goForward(){
+        webView.goForward()
+        
+        if let url = webView.url {
+            urlString = url.absoluteString
+        }
+    }
+    
+    func reload() {
+        webView.reload()
+        
+        webView.evaluateJavaScript(
+"""
+console.log(":D")
+""")
+    }
+    
+    
+    func loadURL(_ loadString: String) {
+        urlString = loadString
+        
+        webView.load(URLRequest(url: self.createURL(loadString)!))
+    }
+    
+    func createURL(_ loadString: String) -> URL? {
+        if urlString.contains(protocolRegex) {
+            return URL(string: urlString)
+        } else if urlString.contains(domainRegex) {
+            return URL(string: "https://\(urlString)")
+        } else {
+           return URL(string: "https://ecosia.org/search?q=\(urlString)")
+        }
+    }
+    
+    func loadURL(_ loadURL: URL) {
+        urlString = loadURL.absoluteString
+        
+        webView.load(URLRequest(url: loadURL))
+    }
+}
+
+#endif
