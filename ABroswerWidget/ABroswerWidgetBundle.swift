2f22e7389c95d73c4e97036187b94b5b453bda02
ljp-projects <lucasplumb@icloud.com>
2024-04-12 07:14:07 +1000

    stuff

---
+++ ABroswerWidget/ABroswerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ABroswerWidgetBundle.swift
+//  ABroswerWidget
+//
+//  Created by Lucas on 6/4/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ABroswerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ABroswerWidget()
+    }
+}
---
+++ ABrowser/ABrowserExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ABrowserExtension.swift
+//  ABrowser
+//
+//  Created by Lucas on 7/4/2024.
+//
+
+import Cocoa
+
+class ABrowserExtension: NSObject {
+
+}
